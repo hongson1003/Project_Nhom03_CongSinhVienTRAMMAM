0c35fdfe2aa5b41bdcc62ab6c18c813c507aa089
Huy Trần Lê <tranlehuy0407@gmail.com>
2024-04-19 15:54:28 +0700

    minor update

--- AuthenticationService/src/routes/index.js
+++ AuthenticationService/src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const configRoutes = async (app) => {
     app.get('/', (req, res) => {
-        return res.status(200).json({ message: 'Welcome to the chat app' });
+        return res.status(200).json({ message: 'Welcome to the TRAMMAM university' });
     })
 }
 
